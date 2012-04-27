37a3f4163855b8f0db0ebe5650b536119e850c75
Robert McLeod <hamstar@behemoth.(none)>
2012-04-28 00:07:09 +1200

    vanilla rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Legislator::Application.config.secret_token = '716a18fc62ea2f417c79e872f1398204acda9ea0f511c9cffe3f76441f2d3da31ff5ad526f605f7c2daee234557fc206db7a04913c714de3a90ad77f0cf2e616'
