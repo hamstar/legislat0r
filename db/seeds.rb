2b6c76d8b0b9c341b90afe716ca760548a41270e
Robert McLeod <hamstar@behemoth.(none)>
2012-04-29 13:59:25 +1200

    some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+bill = Bill.create({name: "Basic Template"})
+
+Revision.create({
+  section: Section.create({
+    bill_id: bill.id,
+    title: "Title 1: Problems and Grievances"
+  }),
+  markup: "This title should explain the current situation and the problems"
+})
+
+Revision.create({
+  section: Section.create({
+    bill_id: bill.id,
+    title: "Title 2: Glossary of Terms"
+  }),
+  markup: "This title should explain the domain language/definitions"
+})
+
+Revision.create({
+  section: Section.create({
+    bill_id: bill.id,
+    title: "Title 3: Roles, Responsibilities and Expectations"
+  }),
+  markup: "This title should explain the Roles, Responsibilities and Expectations of each stakeholder in the document"
+})
+
+Revision.create({
+  section: Section.create({
+    bill_id: bill.id,
+    title: "Title 4: Guiding Principles"
+  }),
+  markup: "This title should explain the principles that guide this document"
+})
+
+Revision.create({
+  section: Section.create({
+    bill_id: bill.id,
+    title: "Title 5: Solutions"
+  }),
+  markup: "This title should explain the solutions. Each solution traces to one or more principles to alleviate or minimize one or more problem/grievance."
+})
