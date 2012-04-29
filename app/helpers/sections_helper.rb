8d5e6c2da10144d83bba302caa0b9ab324d16489
Robert McLeod <hamstar@behemoth.(none)>
2012-04-29 12:29:55 +1200

    added redcarpet and a markdown helper (currently not working)

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -8,4 +8,18 @@ module SectionsHelper
     end
   end
 
+  def markdown(text)
+  	text
+    # renderer = Redcarpet::Render::HTML.new({
+    #   :filter_html => true,
+    #   :hard_wrap => true
+    # })
+    # markdown = Redcarpet::Markdown.new(renderer, {
+    #   :autolink => true,
+    #   :no_intra_emphasis => true
+    # })
+
+    # markdown.render(text).html_safe
+  end
+
 end
---
+++ config/initializers/redcarpet.rb
@@ -0,0 +1,3 @@
+#Markdown = Redcarpet::Markdown.new( Redcarpet::Render::HTML([:hard_wrap, :filter_html, :autolink]) )
+
+require 'redcarpet'
